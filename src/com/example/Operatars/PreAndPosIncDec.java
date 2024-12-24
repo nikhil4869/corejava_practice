d55d835ebb48cbbf84e89392e949ea2a9f4080eb
nikhil4869 <bchanduvasu4869@gmail.com>
2024-12-24 14:09:58 +0530

    pre and post increment and decrement

---
+++ src/com/example/Operatars/PreAndPosIncDec.java
@@ -0,0 +1,17 @@
+package com.example.Operatars;
+
+public class PreAndPosIncDec {
+    public static void main(String[] args) {
+        int a=10;
+        int b=a++;
+        int c=++b;
+        int d=--a;
+        int e= d--;
+        System.out.println("print a value" + a);
+        System.out.println("print b value" + b);
+        System.out.println("print c value" + c);
+        System.out.println("print d value" + d);
+        System.out.println("print e value" + e);
+
+    }
+}
